c78ca99d9a38b5c54d6cd6be03529c4abaa99012
ctcaulfield <ctcaulfield@gmail.com>
2018-10-22 20:37:35 -0400

    pi

--- pictureServer.js
+++ pictureServer.js
@@ -129,7 +129,7 @@ io.on('connect', function(socket) {
     NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
     io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
     /// The browser will take this new name and load the picture from the public folder.
-    client.faceDetection(imageName+'.jpg').then(results => {
+    client.faceDetection('public/'+imageName+'.jpg').then(results => {
       const faces = results[0].faceAnnotations;
       const numFaces = faces.length;
       console.log('Found ' + numFaces + (numFaces === 1 ? ' face' : ' faces'));
